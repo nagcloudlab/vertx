5d984226fc1ab68ba3decda43fdfaaac55fbea0e
Nagabhushanam <nagabhushanamn@gmail.com>
2020-12-17 11:18:54 +0530

    wiki

---
+++ code-backup/wiki/vertx-wiki-v4/src/main/java/io/vertx/guides/wiki/MainVerticle.java
@@ -0,0 +1,32 @@
+package io.vertx.guides.wiki;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Promise;
+import io.vertx.guides.wiki.database.WikiDatabaseVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+	
+
+	  @Override
+	  public void start(Promise<Void> promise) throws Exception {
+
+	    Promise<String> dbVerticleDeployment = Promise.promise();  // <1>
+	    vertx.deployVerticle(new WikiDatabaseVerticle(), dbVerticleDeployment);  // <2>
+
+	    dbVerticleDeployment.future().compose(id -> {  // <3>
+
+	      Promise<String> httpVerticleDeployment = Promise.promise();
+	      vertx.deployVerticle(
+	        "io.vertx.guides.wiki.http.HttpServerVerticle",  // <4>
+	        new DeploymentOptions().setInstances(2),    // <5>
+	        httpVerticleDeployment);
+
+	      return httpVerticleDeployment.future();  // <6>
+
+	    })
+	    .onSuccess(v->promise.complete())
+	    .onFailure(e->promise.fail("failed"));
+	  }
+
+}
---
+++ code-backup/wiki/vertx-wiki-v4/src/main/java/io/vertx/guides/wiki/database/SqlQuery.java
@@ -0,0 +1,5 @@
+package io.vertx.guides.wiki.database;
+
+public enum SqlQuery {
+	CREATE_PAGES_TABLE, ALL_PAGES, GET_PAGE, CREATE_PAGE, SAVE_PAGE, DELETE_PAGE,ALL_PAGES_DATA
+}
