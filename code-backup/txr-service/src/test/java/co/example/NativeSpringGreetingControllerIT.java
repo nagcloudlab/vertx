c54909a1678155c129aaea39d34370c2d2ae4afd
Nagabhushanam <nagabhushanamn@gmail.com>
2020-12-18 15:30:56 +0530

    txr service

---
+++ code-backup/txr-service/src/test/java/co/example/NativeSpringGreetingControllerIT.java
@@ -0,0 +1,9 @@
+package co.example;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeSpringGreetingControllerIT extends SpringGreetingControllerTest {
+
+    // Execute the same tests but in native mode.
+}
